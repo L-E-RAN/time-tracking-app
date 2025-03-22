04a73e3f22c9939603f80160ba64f8905247c159
L-E-RAN <eliran.ashwal@gmail.com>
2025-03-22 22:57:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ function App() {
 
   return (
     <>
-      <header>
-        <img src="/logo192.png" alt="Logo" style={{ height: 32, verticalAlign: "middle", marginRight: 10 }} />
-        Time Tracking App
+      <header className="header">
+        <div className="header-content">
+          <div className="header-title">
+            <img
+              src="/logo192.png"
+              alt="Logo"
+              style={{ height: 32, verticalAlign: "middle", marginLeft: 10 }}
+            />
+            Time Tracking App
+          </div>
+          {user && (
+            <button className="logout-btn" onClick={() => setUser(null)}>
+              התנתקות
+            </button>
+          )}
+        </div>
       </header>
+
       <main>
         {!user ? (
           <div className="container">
@@ -19,8 +33,6 @@ function App() {
           </div>
         ) : (
           <div className="container">
-            <h1>המשימות היומיות שלך</h1>
-            <button onClick={() => setUser(null)}>Logout</button>
             <TaskTracker user={user} />
           </div>
         )}
